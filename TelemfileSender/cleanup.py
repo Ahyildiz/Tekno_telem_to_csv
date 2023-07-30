277898005700626fe2c3287bfc3d5c77e73b3d13
Ahyildiz <ahyildiz15@gmail.com>
2023-07-30 19:10:16 +0300

    update on csv cleanup.py

---
+++ TelemfileSender/cleanup.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+
+def remove_constant_increase_from_end(filename):
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv(filename)
+
+    #do this until the last row is not the same as the row before it
+    while df.tail(1)['iha_enlem'].values == df.iloc[-2]['iha_enlem'] and df.tail(1)['iha_boylam'].values == df.iloc[-2]['iha_boylam'] and df.tail(1)['iha_irtifa'].values == df.iloc[-2]['iha_irtifa'] and df.tail(1)['iha_dikilme'].values == df.iloc[-2]['iha_dikilme'] and df.tail(1)['iha_yonelme'].values == df.iloc[-2]['iha_yonelme'] and df.tail(1)['iha_yatis'].values == df.iloc[-2]['iha_yatis']:
+        #drop the last row
+        df.drop(df.tail(1).index, inplace=True)
+        #save the new csv file
+        df.to_csv(filename, index=False)
+        print("Last row dropped")
+
+
+if __name__ == "__main__":
+
+    file_path = "C:\\Users\\ahyil\\PycharmProjects\\Tekno_telem_to_csv"
+
+    for root, _, files in os.walk(file_path):
+        print(f"Processing {root}...")
+        for filename in files:
+            if filename.endswith(".csv"):
+                file_path = os.path.join(root, filename)
+                remove_constant_increase_from_end(file_path)
+
+
--- TelemfileSender/future_prediction.py
+++ TelemfileSender/future_prediction.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#csv_directory = "C:\\Users\\ahyil\\PycharmProjects\\Tekno_telem_to_csv\\TelemfileSender\\cleaned_files"
 csv_directory = "C:\\Users\\ahyil\\PycharmProjects\\Tekno_telem_to_csv"
-
 # Iterate through each file in the directory
 for filename in os.listdir(csv_directory):
     if filename.endswith(".csv"):
@@ -16,10 +16,10 @@ for filename in os.listdir(csv_directory):
         plt.figure(figsize=(10, 5))
         plt.plot(df['timestamp'], df['zaman_farki'])
         plt.xlabel('Timestamp')
-        plt.ylabel('Aircraft Latitude')
-        plt.title(f'Aircraft Latitude Over Time - {filename}')
+        plt.ylabel('Delay (ms)')
+        plt.title(f'Airplane Delay Plot for {filename}')
         plt.grid(True)
-        plt.savefig(f'aircraft_latitude_plot_{filename}.png')  # Save the plot as an image file
+        plt.savefig(f'{filename}_delay_plot.png')
         plt.close()  # Close the plot to avoid overlapping in the next iteration
 
 print("Plots generated for all CSV files!")
